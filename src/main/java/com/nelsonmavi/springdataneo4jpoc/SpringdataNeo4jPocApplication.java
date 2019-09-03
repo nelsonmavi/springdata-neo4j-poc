1c6e4df5b0cbc6a2fe882d8be09b678e23ea1a5b
Nelson <nelsonmavi@gmail.com>
2019-09-03 22:06:51 +0200

    Created SpringBoot project

---
+++ src/main/java/com/nelsonmavi/springdataneo4jpoc/SpringdataNeo4jPocApplication.java
@@ -0,0 +1,13 @@
+package com.nelsonmavi.springdataneo4jpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataNeo4jPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataNeo4jPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelsonmavi/springdataneo4jpoc/SpringdataNeo4jPocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nelsonmavi.springdataneo4jpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringdataNeo4jPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
